96508b6ea87378bd2eed2430996273304936fced
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2025-01-17 15:41:52 +0530

    logged in user and logout functionality

--- Firebase-1/src/App.jsx
+++ Firebase-1/src/App.jsx
@@ -1,24 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getDatabase, ref, set } from "firebase/database";
 import { app } from "./firebase";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";// authetication
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "firebase/auth"; // authetication
 import Signup from "./Pages/Signup";
 import SignIn from "./Pages/SignIn";
-
-//get an instance for authentication
-
-{/*const auth = getAuth(app);
-
-const signUp = () =>{createUserWithEmailAndPassword(
-  auth,"shanatnu@gmail.com",
-  "shantanu123344"
-).then(value => console.log(value) //it returns an promise
-)}
-;*/}
-
-
-
 //realtimedatabase===
 
 /*get an instance of database
@@ -35,21 +26,39 @@ const putData = () => {
   }); // in set function we have to pass an refrence of database.In 1st parameter a refrence of database and 2nd parameter is your name of your root where the data is been stored
 };*/
 
+const auth = getAuth(app);
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setuser] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("hello", user);
+        setuser(user);
+      } else {
+        console.log("user not logged in");
+        setuser(null);
+      }
+    });
+  }, []);
+  //conditonal renderring
+  if (user === null) {
+    return (
+      <>
+        <Signup />
+        <SignIn />
+      </>
+    );
+  }
 
   return (
     <>
-     {/* <h1> Firebase Project</h1>
+      {/* <h1> Firebase Project</h1>
       <button onClick={putData}>send Data</button>*/}
 
-
-{/*<h1> Firebase Project</h1>
-<button onClick={signUp}>create user email</button>
-    */}  
-<Signup/>
-<SignIn/>
-    
+      <p> hello {user.email}</p>
+      <button onClick={()=>signOut(auth)}>logout</button>
     </>
   );
 }
