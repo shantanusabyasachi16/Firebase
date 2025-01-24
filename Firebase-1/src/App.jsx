ca72f1c4df9146eb0b7e147ce9d908c3b0219383
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2025-01-24 11:48:13 +0530

    Cloud Firestore

--- Firebase-1/src/App.jsx
+++ Firebase-1/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, query, ref, set } from "firebase/database";
 import { app } from "./firebase";
 import {
   getAuth,
@@ -10,6 +10,9 @@ import {
 } from "firebase/auth"; // authetication
 import Signup from "./Pages/Signup";
 import SignIn from "./Pages/SignIn";
+import {  addDoc, collection, doc, getDoc, getDocs, getFirestore, updateDoc, where} from "firebase/firestore";
+
+
 //realtimedatabase===
 
 /*get an instance of database
@@ -26,12 +29,64 @@ const putData = () => {
   }); // in set function we have to pass an refrence of database.In 1st parameter a refrence of database and 2nd parameter is your name of your root where the data is been stored
 };*/
 
-const auth = getAuth(app);
 
+
+//cloud firestore
+const cloudFirestore = getFirestore(app);
 function App() {
   const [user, setuser] = useState("");
 
-  useEffect(() => {
+  // //cloud firestore
+  const writeData = async()=>{
+ const result= await addDoc(collection(cloudFirestore,'cities'),{
+    Location:"cuttcak",
+   PinCode:768908,
+   lat:344,
+   long:4554
+  })
+console.log(result);
+};
+//puting the sub data in main data
+
+const Subdata = async()=>{
+  const data1 = await addDoc(collection(cloudFirestore,'cities/Eo8q02rT2FI6M79bWBpt/places'),{
+    name:"this bbsr",
+    desc: "this is description",
+    date: Date.now(),
+  })
+};
+
+//to get the data from the collection
+
+const getddocument  = async ()=>{
+  //create a reference
+  const ref =  doc(cloudFirestore,"cities","Eo8q02rT2FI6M79bWBpt");
+  const snap = await  getDoc(ref);
+  console.log(snap.data());
+  
+};
+
+//to get the docummets from query
+const getDocumentbyQuery = async ()=>{
+  //create the refrence
+  const collectionRef= collection(cloudFirestore,"Users");
+  const q = query(collectionRef, where("isFemale" ,"==", true));
+
+  const snapShot = await getDocs(q);//to get all the documents
+//we have to ittrate for aall the docs
+snapShot.forEach((data)=>console.log(data.data()));
+}
+//to update the document
+
+const updatedata = async()=>{
+  const docref = doc(cloudFirestore,'cities','Eo8q02rT2FI6M79bWBpt');
+  await updateDoc(docref,{
+    Location:'Kolkata'
+  })
+}
+
+  {/*const auth = getAuth(app);*/}
+ /* useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         console.log("hello", user);
@@ -50,15 +105,23 @@ function App() {
         <SignIn />
       </>
     );
-  }
+  }*/
 
   return (
     <>
+    {/*database*/}
       {/* <h1> Firebase Project</h1>
       <button onClick={putData}>send Data</button>*/}
 
-      <p> hello {user.email}</p>
-      <button onClick={()=>signOut(auth)}>logout</button>
+      {/*<p> hello {user.email}</p>
+      <button onClick={()=>signOut(auth)}>logout</button>*/}
+
+    <p>cloudstore</p>
+    <button onClick={writeData}>Send Data</button>
+    <button onClick={Subdata}>Send Sub Data</button>
+    <button onClick={getddocument}>get Data</button>
+    <button onClick={getDocumentbyQuery}>get Data by query</button>
+    <button onClick={updatedata }>update</button>
     </>
   );
 }
